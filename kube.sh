50711288c8852054e704f548af595340a283149c
Ezequiel Arielli <earielli@artear.com>
2017-12-12 17:59:11 -0300

    test

--- kube.sh
+++ kube.sh
@@ -7,8 +7,8 @@ echo $REPO:$CIRCLE_TAG
 sed -i "s/<VERSION>/$(echo $CIRCLE_TAG | cut -c 1-7)/" template/deployment.yml
 sed -i "s/<REPO>/$(echo $REPO | cut -c 1-7)/" template/deployment.yml
 sed -i "s/<PROJECT>/$(echo $CIRCLE_PROJECT_REPONAME | cut -c 1-7)/" template/deployment.yml
-sed -i "s/<kube>/$(echo $KUBE_API | cut -c 1-99999)/" ~/.kube/config
-sed -i "s/<certificate-authority-data>/$(echo $CERTIFICATE_AUTHORITY_DATA | cut -c 1-99999)/" ~/.kube/config
-sed -i "s/<client-certificate-data>/$(echo $CLIENT_CERTIFICATE_DATA | cut -c 1-99999)/" ~/.kube/config
-sed -i "s/<client_key_data>/$(echo $CLIENT_KEY_DATA | cut -c 1-99999)/" ~/.kube/config
+sed -i "s/<kube>/$(echo $KUBE_API | cut -c 1-99999)/" /home/ubuntu/.kube/config
+sed -i "s/<certificate-authority-data>/$(echo $CERTIFICATE_AUTHORITY_DATA | cut -c 1-99999)/" /home/ubuntu/.kube/config
+sed -i "s/<client-certificate-data>/$(echo $CLIENT_CERTIFICATE_DATA | cut -c 1-99999)/" /home/ubuntu/.kube/config
+sed -i "s/<client_key_data>/$(echo $CLIENT_KEY_DATA | cut -c 1-99999)/" /home/ubuntu/.kube/config
 
